8ad67cc055cba2e29c5984f325343c569d5bbe8f
x_air <unchainedsoul@nate.com>
2024-07-29 20:20:43 +0900

    Initial Commit

---
+++ x_prompt/x_promptApp.swift
@@ -0,0 +1,17 @@
+//
+//  x_promptApp.swift
+//  x_prompt
+//
+//  Created by MIN GI KIM on 7/29/24.
+//
+
+import SwiftUI
+
+@main
+struct x_promptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
